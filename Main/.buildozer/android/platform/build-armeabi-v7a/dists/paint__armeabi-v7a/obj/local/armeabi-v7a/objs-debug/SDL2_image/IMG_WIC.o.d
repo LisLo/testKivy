6c646946ae2175863425c06e08e93260843ccd06
David <lisaloerinci@gmail.com>
2021-10-14 10:59:33 +0200

    test

--- Main/.buildozer/android/platform/build-armeabi-v7a/dists/paint__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/SDL2_image/IMG_WIC.o.d
+++
@@ -1,2 +0,0 @@
-/home/lisa/Dokumente/GitHub/testKivy/Main/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2_image/IMG_WIC.o: \
-  /home/lisa/Dokumente/GitHub/testKivy/Main/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/IMG_WIC.c
--- Main/.buildozer/android/platform/build-armeabi-v7a/dists/paint__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/lisa/Dokumente/GitHub/testKivy/Main/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/lisa/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.c \
-  /home/lisa/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h
-
-/home/lisa/.buildozer/android/platform/android-ndk-r19c/sources/android/cpufeatures/cpu-features.h:
